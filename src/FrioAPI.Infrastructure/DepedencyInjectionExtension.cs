7ca96d0bb88e969668fb36af22e12799619b162b
ArthurTorres1 <arthurtorres059@gmail.com>
2025-04-13 10:22:41 -0300

    Registrando DbContext como injeção de dependência.

--- src/FrioAPI.Infrastructure/DepedencyInjectionExtension.cs
+++ src/FrioAPI.Infrastructure/DepedencyInjectionExtension.cs
@@ -1,4 +1,5 @@
 ﻿using FrioAPI.Domain.Repositories.Recibos;
+using FrioAPI.Infrastructure.DataAccess;
 using FrioAPI.Infrastructure.DataAccess.Repositories;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -7,8 +8,18 @@ namespace FrioAPI.Infrastructure
     public static class DepedencyInjectionExtension
     {
         public static void AddInfrastructure(this IServiceCollection services)
-        { 
+        {
+            AddRepostories(services);
+            AddDbContext(services);
+        }
+        private static void AddRepostories(IServiceCollection services)
+        {
             services.AddScoped<IRecibosRepository, RecibosRepository>();
         }
+        private static void AddDbContext(IServiceCollection services)
+        {
+            services.AddDbContext<FrioApiDBContext>();
+        }
+
     }
 }
