be7b1279733ebb3857499d310f063254fa0e0c59
ArthurTorres1 <arthurtorres059@gmail.com>
2025-04-15 23:46:50 -0300

    adicionando mapeamento com nuget package automapper.

--- src/FrioAPI.Api/Controllers/RecibosController.cs
+++ src/FrioAPI.Api/Controllers/RecibosController.cs
@@ -1,11 +1,14 @@
 ﻿using FrioAPI.Application.UseCases.Recibos.Register;
 using FrioAPI.Communication.Requests;
+using FrioAPI.Communication.Responses;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FrioAPI.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [ProducesResponseType(typeof(ResponseRegisteredReciboJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
     public class RecibosController : ControllerBase
     {
         [HttpPost]
--- src/FrioAPI.Application/DependencyInjectionExtension.cs
+++ src/FrioAPI.Application/DependencyInjectionExtension.cs
@@ -1,4 +1,5 @@
-﻿using FrioAPI.Application.UseCases.Recibos.Register;
+﻿using FrioAPI.Application.AutoMapper;
+using FrioAPI.Application.UseCases.Recibos.Register;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace FrioAPI.Application
@@ -6,6 +7,17 @@ namespace FrioAPI.Application
     public static class DependencyInjectionExtension
     {
         public static void AddApplication(this IServiceCollection services)
+        {
+            AddUseCases(services);
+            AddAutoMapper(services);
+        }
+
+        private static void AddAutoMapper(IServiceCollection services)
+        {
+            services.AddAutoMapper(typeof(AutoMapping));
+        }
+
+        private static void AddUseCases(IServiceCollection services)
         {
             services.AddScoped<IRegisterReciboUseCase, RegisterReciboUseCase>();
         }
